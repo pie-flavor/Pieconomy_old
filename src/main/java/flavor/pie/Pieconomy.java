ae5f8a148c74756bf497c5bff82eb048d7637513
Adam Spofford <pieflavor.mc@gmail.com>
2016-05-01 04:14:27 -0700

    fixed a hocon whoopsie

--- src/main/java/flavor/pie/Pieconomy.java
+++ src/main/java/flavor/pie/Pieconomy.java
@@ -39,7 +39,7 @@ import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 
-@Plugin(id="pieconomy",name="Pieconomy", authors="pie_flavor", description="An economy plugin that uses items as currency", version="1.0.0")
+@Plugin(id="pieconomy",name="Pieconomy", authors="pie_flavor", description="An economy plugin that uses items as currency", version="1.0.1")
 public class Pieconomy {
 	@Inject Game game;
 	@Inject @ConfigDir(sharedRoot = false) File dir;
